f32ada9d36b1b85d3833238f06d6fa0de7523059
Alginatic <203040094@mail.unpas.ac.id>
2022-10-13 21:15:20 +0700

    rev

--- phpmvc/app/views/about/index.php
+++ phpmvc/app/views/about/index.php
@@ -1,3 +1,6 @@
+<!-- Rizky Mahendra
+203040094 -->
+
 <div class="container">
 <h1 class="mt-4">About</h1>
     <img src="<?= BASEURL ?>../img/profile.jpeg" alt="Rizky Mahendra" width="200" class="rounded-circle shadow">
